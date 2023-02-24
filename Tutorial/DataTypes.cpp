c822e03188a9ba39d2b0c1414eca69f3f9ed365f
Tiago Chacon <tiagochacon80@gmail.com>
2023-02-24 14:24:01 -0500

    Basic data types

---
+++ Tutorial/DataTypes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int myNum = 5;
+    float myFloatNum = 5.99;
+    double myDoubleNum = 9.98;
+    char myLetter = 'D';
+    bool myBoolean = true;
+    string myText = "Hello";
+    return 0;
+
+    /*float vs. double
+    The precision of a floating point value indicates how many digits the value can have after the decimal point. 
+    The precision of float is only six or seven decimal digits, while double variables have a precision of about 15 digits. 
+    Therefore it is safer to use double for most calculations.*/
+}
