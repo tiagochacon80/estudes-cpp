91eb7a20ddf3d7a344903634804556df59c48418
Tiago Chacon <tiagochacon80@gmail.com>
2023-02-23 19:34:07 -0500

    Declare variables

---
+++ Tutorial/DeclareVariables.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    //Declare à variables
+    int myNum = 15;
+    cout << myNum;
+
+    //Declare Multiple Variables
+    int x = 5, y = 6, z = 50;
+    cout << x + y + z;
+
+    //Constants
+    const int myNumConstant = 15;
+    // myNumConstant = 10; error: assignment of read-only variable 'myNum'
+
+}
+
+
+
