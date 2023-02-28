b99ce4e572da0413c141da4aa7a9fe2ae4accfe6
Tiago Chacon <tiagochacon80@gmail.com>
2023-02-28 16:36:26 -0500

    Some math Functions

---
+++ Tutorial/MathFunction.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+    cout << sqrt(64) << "\n";
+    cout << round(2.6) << "\n";
+    cout << log(2) << "\n";
+    cout << pow(2, 2) << "\n";
+    cout << fmax(10, 1) << "\n";
+    cout << fmin(100, 350) << "\n";
+    return 0;
+}
